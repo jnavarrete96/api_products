4e8607ff00c979c1ce7c6527ca3f9e638f1ce21e
Juan Pablo Navarrete <juan.navarrete@agenciamomentum.co>
2025-09-07 15:19:15 -0500

    agregar libreria cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 import productRoutes from './routes/productRoutes';
 import { swaggerDocs } from './config/swagger';
 import { errorHandler } from './middlewares/errorHandler';
@@ -6,6 +7,7 @@ import { errorHandler } from './middlewares/errorHandler';
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use('/api', productRoutes);
 swaggerDocs(app);
 app.use(errorHandler);
